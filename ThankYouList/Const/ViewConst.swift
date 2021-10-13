ee70f8eb7d536131310b24d1830a0676ecd6fc3b
Aika Yamada <balenaik@gmail.com>
2021-10-14 05:44:30 +0900

    Setup Skeleton configuration on ListSectionHeaderView

---
+++ ThankYouList/Const/ViewConst.swift
@@ -0,0 +1,13 @@
+//
+//  ViewConst.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2021/10/14.
+//  Copyright © 2021 Aika Yamada. All rights reserved.
+//
+
+import Foundation
+
+enum ViewConst {
+    static let skeletonViewFadeTime = TimeInterval(0.25)
+}

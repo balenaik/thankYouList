849a4685ea71fbe9d521ace6e17f00b6a70dd39c
Aika Yamada <balenaik@gmail.com>
2021-11-02 00:57:30 +0900

    Add ThankYouCellTapMenu

---
+++ ThankYouList/Presenter/Common/ThankYouCellTapMenu.swift
@@ -0,0 +1,36 @@
+//
+//  ThankYouCellTapMenu.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2021/11/01.
+//  Copyright © 2021 Aika Yamada. All rights reserved.
+//
+
+import UIKit
+
+enum ThankYouCellTapMenu: Int, CaseIterable {
+    case edit
+    case delete
+
+    var title: String {
+        switch self {
+        case .edit: return R.string.localizable.editThankYou()
+        case .delete: return R.string.localizable.delete()
+        }
+    }
+
+    var image: UIImage? {
+        switch self {
+        case .edit: return R.image.icEdit24()
+        case .delete: return R.image.icDelete24()
+        }
+    }
+}
+
+extension ThankYouCellTapMenu {
+    var bottomHalfSheetMenuItem: BottomHalfSheetMenuItem {
+        return BottomHalfSheetMenuItem(title: self.title,
+                                       image: self.image,
+                                       rawValue: self.rawValue)
+    }
+}

dc9731cff0602de6e4186890df2c34306b89f886
Aika Yamada <balenaik@gmail.com>
2022-05-14 11:47:31 +0900

    Add MyPageButtonCell

---
+++ ThankYouList/Presenter/MyPage/MyPageButtonCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyPageButtonCell.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2022/05/14.
+//  Copyright © 2022 Aika Yamada. All rights reserved.
+//
+
+import UIKit
+
+class MyPageButtonCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

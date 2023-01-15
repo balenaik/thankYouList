738ee49ea31c5fdf2aacecd851f3b33dde2b3fa7
Aika Yamada <balenaik@gmail.com>
2023-01-15 15:13:29 +0900

    Add common alert logic on Coordinator

--- ThankYouList/Supports/Router.swift
+++ ThankYouList/Supports/Router.swift
@@ -9,4 +9,34 @@
 import UIKit
 
 protocol Router {
+    func presentAlert(title: String?,
+                      message: String?,
+                      actions: [UIAlertAction]?)
+}
+
+extension Router {
+    func presentAlert(title: String? = nil,
+                      message: String? = nil,
+                      actions: [UIAlertAction]? = nil) {
+        presentAlert(title: title, message: message, actions: actions)
+    }
+}
+
+// MARK: - Router protocol conforming by Coordinator
+extension Coordinator {
+    func presentAlert(title: String?, message: String?, actions: [UIAlertAction]?) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        if let actions = actions, !actions.isEmpty {
+            actions.forEach {
+                alertController.addAction($0)
+            }
+        } else {
+            let okAction = UIAlertAction(title: R.string.localizable.ok(), style: .default)
+            alertController.addAction(okAction)
+        }
+
+        viewController?.present(alertController, animated: true)
+    }
 }

2332b9a8de12a17ad6b1c64b9b1ddfa2b92f9e59
Aika Yamada <balenaik@gmail.com>
2023-03-25 23:03:35 +0900

    Move randomNonce method to String extension

---
+++ ThankYouList/Extensions/String+Nonce.swift
@@ -0,0 +1,43 @@
+//
+//  String+Nonce.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2023/03/14.
+//  Copyright © 2023 Aika Yamada. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static func randomNonce(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+
+        return result
+    }
+}
--- ThankYouList/Presenter/Login/LoginViewController.swift
+++ ThankYouList/Presenter/Login/LoginViewController.swift
@@ -71,7 +71,7 @@ extension LoginViewController {
     }
 
     @IBAction func tapAppleLoginButton(_ sender: Any) {
-        currentNonce = randomNonceString()
+        currentNonce = String.randomNonce()
         let appleIDProvider = ASAuthorizationAppleIDProvider()
         let request = appleIDProvider.createRequest()
         request.requestedScopes = [.fullName, .email]
@@ -117,38 +117,6 @@ private extension LoginViewController {
 
     func updateUserEmail(email: String) {
         Auth.auth().currentUser?.updateEmail(to: email, completion: nil)
-    }
-
-    func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
-        let charset: Array<Character> =
-            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        var result = ""
-        var remainingLength = length
-
-        while remainingLength > 0 {
-            let randoms: [UInt8] = (0 ..< 16).map { _ in
-                var random: UInt8 = 0
-                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                if errorCode != errSecSuccess {
-                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-                }
-                return random
-            }
-
-            randoms.forEach { random in
-                if remainingLength == 0 {
-                    return
-                }
-
-                if random < charset.count {
-                    result.append(charset[Int(random)])
-                    remainingLength -= 1
-                }
-            }
-        }
-
-        return result
     }
 }
 

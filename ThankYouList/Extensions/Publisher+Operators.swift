ad55aaab30c1e6de06491464a922f648772b357c
Aika Yamada <balenaik@gmail.com>
2023-03-26 11:22:45 +0900

    Add asFuture operator

--- ThankYouList/Extensions/Publisher+Operators.swift
+++ ThankYouList/Extensions/Publisher+Operators.swift
@@ -69,4 +69,27 @@ extension Publisher {
             }
         }.eraseToAnyPublisher()
     }
+
+    func asFuture() -> Future<Output, Failure> {
+        return Future { promise in
+            var ticket: AnyCancellable?
+            ticket = self.sink(
+                receiveCompletion: {
+                    ticket?.cancel()
+                    ticket = nil
+                    switch $0 {
+                    case let .failure(error):
+                        promise(.failure(error))
+                    case .finished:
+                        break
+                    }
+                },
+                receiveValue: {
+                    ticket?.cancel()
+                    ticket = nil
+                    promise(.success($0))
+                }
+            )
+        }
+    }
 }

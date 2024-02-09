187012d98cb3141249f84f83ea59c6f6e5f3653d
Aika Yamada <balenaik@gmail.com>
2024-02-09 21:16:50 +0900

    Add AddPositiveStatementView

---
+++ ThankYouList/Presenter/AddPositiveStatement/AddPositiveStatementView.swift
@@ -0,0 +1,19 @@
+//
+//  AddPositiveStatementView.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2024/02/09.
+//  Copyright © 2024 Aika Yamada. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddPositiveStatementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPositiveStatementView()
+}

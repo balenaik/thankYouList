802708bb0f8af2695e524167f9362125ace9a33e
Aika Yamada <balenaik@gmail.com>
2024-02-12 17:15:52 +0900

    Add NavigationBarTitle on Add PositiveStatementView

--- ThankYouList/Presenter/AddPositiveStatement/AddPositiveStatementView.swift
+++ ThankYouList/Presenter/AddPositiveStatement/AddPositiveStatementView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct AddPositiveStatementView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text(
+                "Hello, World!"
+            )
+            .navigationBarTitle(R.string.localizable.add_positive_statement_title(), displayMode: .inline)
+        }
     }
 }
 

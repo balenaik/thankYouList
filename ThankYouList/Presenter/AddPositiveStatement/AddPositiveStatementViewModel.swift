dcc0003be2d4ad240161f52b645178b83cdb5bb9
Aika Yamada <balenaik@gmail.com>
2024-03-20 09:51:35 +0900

    Add AddPositiveStatementViewModel

---
+++ ThankYouList/Presenter/AddPositiveStatement/AddPositiveStatementViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  AddPositiveStatementViewModel.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2024/03/06.
+//  Copyright © 2024 Aika Yamada. All rights reserved.
+//
+
+import Combine
+
+class AddPositiveStatementViewModel: ObservableObject {
+
+    let inputs = Inputs()
+    let outputs = Outputs()
+    @Published var bindings = Bindings()
+
+    private var cancellable = Set<AnyCancellable>()
+
+    init() {
+        bind()
+    }
+}
+
+private extension AddPositiveStatementViewModel {
+    func bind() {
+        bindings.$textFieldText
+            .sink { aaa in
+                debugPrint(aaa)
+            }
+            .store(in: &cancellable)
+    }
+}
+
+extension AddPositiveStatementViewModel {
+    class Inputs {
+    }
+
+    class Outputs {
+    }
+
+    class Bindings: ObservableObject {
+        @Published var textFieldText = ""
+    }
+}

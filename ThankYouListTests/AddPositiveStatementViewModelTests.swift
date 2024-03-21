f74e79f5e05906fa4698665b3b55400d843cdd3d
Aika Yamada <balenaik@gmail.com>
2024-03-21 13:32:56 +0900

    Add AddPositiveStatementViewModelTests

---
+++ ThankYouListTests/AddPositiveStatementViewModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  AddPositiveStatementViewModelTests.swift
+//  ThankYouListTests
+//
+//  Created by Aika Yamada on 2024/03/21.
+//  Copyright © 2024 Aika Yamada. All rights reserved.
+//
+
+import XCTest
+import Combine
+import CombineSchedulers
+@testable import ThankYouList
+
+final class AddPositiveStatementViewModelTests: XCTestCase {
+
+    private var viewModel: AddPositiveStatementViewModel!
+
+    override func setUp() {
+        viewModel = AddPositiveStatementViewModel()
+    }
+}

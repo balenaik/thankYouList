32c273f1caf9219f94cec21e69f18586251a3efc
Aika Yamada <balenaik@gmail.com>
2024-04-08 07:32:01 +0900

    Add cancelButtonToolbar extension

---
+++ ThankYouList/Extensions/View+Toolbar.swift
@@ -0,0 +1,23 @@
+//
+//  View+Toolbar.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2024/04/06.
+//  Copyright © 2024 Aika Yamada. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+extension View {
+    func cancelButtonToolbar(_ buttonTapAction: @escaping () -> Void) -> some View {
+        self.toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button(action: buttonTapAction) {
+                    Image(R.image.icCancel)
+                        .foregroundColor(.text)
+                }
+            }
+        }
+    }
+}

5f1ea6547fe8edc85d0466b96f959fcd2131ad15
Aika Yamada <balenaik@gmail.com>
2024-06-15 15:35:10 +0900

    Add SFSymbolConst

---
+++ ThankYouList/Const/SFSymbolConst.swift
@@ -0,0 +1,12 @@
+//
+//  SFSymbolConst.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2024/06/15.
+//  Copyright © 2024 Aika Yamada. All rights reserved.
+//
+
+enum SFSymbolConst {
+    static let lightbulb = "lightbulb"
+    static let chevronRight = "chevron.right"
+}

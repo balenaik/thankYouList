f92216291b4e7262220ba99a6a2449d35b4e49d9
Aika Yamada <balenaik@gmail.com>
2024-11-04 14:21:59 +0900

    Add const files on main target and Widget target

---
+++ ThankYouList/Const/AppConst.swift
@@ -0,0 +1,12 @@
+//
+//  AppConst.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2024/10/27.
+//  Copyright © 2024 Aika Yamada. All rights reserved.
+//
+
+enum AppConst {
+    static let teamId = "6JDKJKDJ28"
+    static let appAccessGroupName = "com.balenaik.ThankYouList"
+}

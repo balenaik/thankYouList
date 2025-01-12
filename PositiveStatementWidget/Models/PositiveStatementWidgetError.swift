76a1afa0044c431f27a1963e701d098969ab9463
Aika Yamada <balenaik@gmail.com>
2025-01-12 12:24:12 +0900

    Add PositiveStatementWidgetError type

---
+++ PositiveStatementWidget/Models/PositiveStatementWidgetError.swift
@@ -0,0 +1,12 @@
+//
+//  PositiveStatementWidgetError.swift
+//  PositiveStatementWidgetExtension
+//
+//  Created by Aika Yamada on 2025/01/12.
+//  Copyright © 2025 Aika Yamada. All rights reserved.
+//
+
+enum PositiveStatementWidgetError: Error {
+    case currentUserNotExist
+    case dataNotFound
+}

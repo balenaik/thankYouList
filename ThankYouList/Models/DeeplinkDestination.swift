97a67937ecd0e2a3de3df2367208e160126d3120
Aika Yamada <balenaik@gmail.com>
2025-02-28 14:38:04 +0900

    Add DeeplinkDestination enum

---
+++ ThankYouList/Models/DeeplinkDestination.swift
@@ -0,0 +1,11 @@
+//
+//  DeeplinkDestination.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2025/02/25.
+//  Copyright © 2025 Aika Yamada. All rights reserved.
+//
+
+enum DeeplinkDestination: String {
+    case positiveStatements
+}

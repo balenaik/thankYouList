94ea0790738ebfeabe5e238daee461b95b711ba0
Aika Yamada <balenaik@gmail.com>
2025-04-07 13:43:17 +0900

    Move Font+Theme into SharedResources

--- SharedResources/Extensions/Font+Theme.swift
+++ SharedResources/Extensions/Font+Theme.swift
@@ -1,6 +1,6 @@
 //
 //  Font+Theme.swift
-//  ThankYouList
+//  SharedResources
 //
 //  Created by Aika Yamada on 2023/01/01.
 //  Copyright © 2023 Aika Yamada. All rights reserved.
@@ -9,11 +9,11 @@
 import SwiftUI
 
 extension Font {
-    static func regularAvenir(ofSize size: CGFloat) -> Font {
+    public static func regularAvenir(ofSize size: CGFloat) -> Font {
         return .custom("Avenir-Book", size: size)
     }
 
-    static func boldAvenir(ofSize size: CGFloat) -> Font {
+    public static func boldAvenir(ofSize size: CGFloat) -> Font {
         return .custom("Avenir-Heavy", size: size)
     }
 }

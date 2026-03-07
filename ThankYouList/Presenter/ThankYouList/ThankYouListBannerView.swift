e983a4a494fc2536724d534a900dd797af05d227
Aika Yamada <balenaik@gmail.com>
2026-03-07 22:43:49 +0900

    Setup banner card's cornerRadius and border

--- ThankYouList/Presenter/ThankYouList/ThankYouListBannerView.swift
+++ ThankYouList/Presenter/ThankYouList/ThankYouListBannerView.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
+private let bannerCornerRadius = CGFloat(16)
+private let bannerBorderColor = UIColor.primary
+private let bannerBorderWidth = CGFloat(1)
+
 class ThankYouListBannerView: UIView {
+
+    @IBOutlet private weak var cardView: UIView!
+
     class func instanceFromNib() -> ThankYouListBannerView {
         let view = R.nib.thankYouListBannerView.firstView(withOwner: nil)!
         return view
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        cardView.layer.cornerRadius = bannerCornerRadius
+        cardView.layer.borderColor = bannerBorderColor.cgColor
+        cardView.layer.borderWidth = bannerBorderWidth
     }
 }

932f9d77d27e366fdcadf2cfdb9b3376fd2030fb
Aika Yamada <balenaik@gmail.com>
2026-03-10 22:27:01 +0900

    Add Banner type

---
+++ ThankYouList/Models/Banner.swift
@@ -0,0 +1,15 @@
+//
+//  Banner.swift
+//  ThankYouList
+//
+//  Created by Aika Yamada on 2026/02/05.
+//  Copyright © 2026 Aika Yamada. All rights reserved.
+//
+
+import UIKit
+
+struct Banner: Equatable {
+    let text: String
+    let buttonText: String
+    let image: UIImage?
+}

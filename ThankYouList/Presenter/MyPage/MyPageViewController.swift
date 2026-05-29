8dde521bbe6969da6c80f3d72b1a726c30980b61
Aika Yamada <balenaik@gmail.com>
2026-05-29 23:17:05 +0900

    Call openLanguageSetting function when language menu is tapped

--- ThankYouList/Presenter/MyPage/MyPageViewController.swift
+++ ThankYouList/Presenter/MyPage/MyPageViewController.swift
@@ -218,7 +218,7 @@ extension MyPageViewController: UITableViewDelegate {
         case .positiveStatements:
             router?.pushToPositiveStatementList()
         case .language:
-            break
+            router?.openLanguageSetting()
         case .rate:
             showRating()
         case .feedback:
